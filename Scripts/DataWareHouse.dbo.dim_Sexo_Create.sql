63164a0c076ae33a42b02cdcf36e5860bad5cc8f
drd0112 <jecasallas1985@gmail.com>
2025-05-27 14:14:44 -0500

    Update DataWareHouse.dbo.dim_Sexo_Create.sql

--- Scripts/DataWareHouse.dbo.dim_Sexo_Create.sql
+++ Scripts/DataWareHouse.dbo.dim_Sexo_Create.sql
@@ -7,5 +7,5 @@
 CREATE TABLE DataWareHouse.dbo.dim_Sexo (
 Id_Sexo tinyint IDENTITY(1,1) NOT NULL,
 Nom_Sexo nvarchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-CONSTRAINT Sexo_PK PRIMARY KEY (Id_Sexo)
+CONSTRAINT dim_Sexo_PK PRIMARY KEY (Id_Sexo)
 );
