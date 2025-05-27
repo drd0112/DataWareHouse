3cd49a9a65d3100ac3cd3f75a0c0326c86c6f96f
drd0112 <jecasallas1985@gmail.com>
2025-05-27 14:14:02 -0500

    Update and rename DataWareHouse.dbo.Dim_Sexo.sql to DataWareHouse.dbo.dim_Sexo_Create.sql

--- Scripts/DataWareHouse.dbo.dim_Sexo_Create.sql
+++ Scripts/DataWareHouse.dbo.dim_Sexo_Create.sql
@@ -1,10 +1,10 @@
--- DataWareHouse.dbo.Dim_Sexo definition
+-- DataWareHouse.dbo.dim_Sexo definition
 
 -- Drop table
 
--- DROP TABLE DataWareHouse.dbo.Dim_Sexo;
+-- DROP TABLE DataWareHouse.dbo.dim_Sexo;
 
-CREATE TABLE DataWareHouse.dbo.Dim_Sexo (
+CREATE TABLE DataWareHouse.dbo.dim_Sexo (
 Id_Sexo tinyint IDENTITY(1,1) NOT NULL,
 Nom_Sexo nvarchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 CONSTRAINT Sexo_PK PRIMARY KEY (Id_Sexo)
