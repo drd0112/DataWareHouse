9b900f472570aaa381510ece0eeebe5835a7d46d
drd0112 <jecasallas1985@gmail.com>
2025-05-27 14:16:10 -0500

    Update and rename DataWareHouse.dbo.Dim_Fecha.sql to DataWareHouse.dbo.dim_Fecha_Create.sql

--- Scripts/DataWareHouse.dbo.dim_Fecha_Create.sql
+++ Scripts/DataWareHouse.dbo.dim_Fecha_Create.sql
@@ -1,10 +1,10 @@
--- DataWareHouse.dbo.Dim_Fecha definition
+-- DataWareHouse.dbo.dim_Fecha definition
 
 -- Drop table
 
--- DROP TABLE DataWareHouse.dbo.Dim_Fecha;
+-- DROP TABLE DataWareHouse.dbo.dim_Fecha;
 
-CREATE TABLE DataWareHouse.dbo.Dim_Fecha (
+CREATE TABLE DataWareHouse.dbo.dim_Fecha (
 Anio int NOT NULL,
 Mes tinyint NOT NULL,
 Dia tinyint NOT NULL,
@@ -12,5 +12,5 @@ Semana tinyint NOT NULL,
 Trimestre tinyint NOT NULL,
 Dia_Semana tinyint NOT NULL,
 Fecha_Hecho date NOT NULL,
-CONSTRAINT Dim_Fecha_PK PRIMARY KEY (Fecha_Hecho)
+CONSTRAINT dim_Fecha_PK PRIMARY KEY (Fecha_Hecho)
 );
